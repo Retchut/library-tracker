453f3bec94436784072dcbf8dd753fcc64bd0d0f
Mário Travassos <retchut@protonmail.ch>
2022-08-31 02:03:23 +0100

    feat: implement loading and saving library

---
+++ src/library.py
@@ -0,0 +1,79 @@
+from dbm.ndbm import library
+from card import Card
+LIBRARY_FILE_NAME = "library.txt"
+
+class Library:
+    collection = list()
+
+    def __init__(self) -> None:
+        pass
+
+    def __repr__(self) -> str:
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+    def getCollection(self) -> list:
+        return self.collection
+    
+    def loadLibrary(self) -> None:
+        try:
+            libraryFile = open(LIBRARY_FILE_NAME, "r")
+        except FileNotFoundError:
+            print("No library file found. Launching with an empty library.")
+        else:
+            print("A library.txt was detected and will be loaded.");
+            success = True;
+            line = 1
+            check = 0
+            for entry in libraryFile:
+                # split entries by token, and remove line terminator from the entry
+                cardData = entry.split("|")
+                cardData[-1] = (cardData[-1])[:-1]
+                self.collection.append(Card(
+                    name=cardData[0],
+                    version=int(cardData[1]),
+                    rarity=cardData[2],
+                    expansion=cardData[3],
+                    condition=cardData[4],
+                    language=cardData[5],
+                    firstEd=bool(cardData[6]),
+                    amount=int(cardData[7]),
+                    price=float(cardData[8])
+                ))
+        libraryFile.close()
+
+    def saveLibrary(self) -> None:
+        with open(LIBRARY_FILE_NAME, "w+") as libraryFile:
+            for card in self.collection:
+                entryString = (card.getName() + "|"
+                        + str(card.getVersion()) + "|"
+                        + card.getRarity() + "|"
+                        + card.getCondition() + "|"
+                        + card.getLanguage() + "|"
+                        + str(card.isFirstEd()) + "|"
+                        + str(card.getAmount()) + "|"
+                        + str(card.getPrice())
+                        + "\n")
+                libraryFile.write(entryString)
+            libraryFile.close()
+
+    def addCard(self) -> int:
+        pass
+
+    def removeCard(self) -> int:
+        pass
+
+    def printLibrary(self) -> None:
+        for card in self.collection:
+            print(card)
+
+    def printSubCollection(self) -> None:
+        pass
+    
+    def accessCard(self) -> int:
+        pass
+
+    def sortLibrary(self, choice : int):
+        pass
---
+++ src/main.py
@@ -0,0 +1,10 @@
+from library import Library
+
+def main():
+    lib = Library()
+    lib.loadLibrary()
+    # lib.printLibrary()
+    lib.saveLibrary()
+
+if __name__ == '__main__':
+    main()
