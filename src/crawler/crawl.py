8c6737e7254668dcc11770c7e9fbad2466088dd7
Mário Travassos <retchut@protonmail.ch>
2023-02-12 00:50:43 +0000

    fix: fix logging and pound price fetching

--- src/crawler/crawl.py
+++ src/crawler/crawl.py
@@ -1,19 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 from re import compile
+from datetime import datetime
+
+CRAWL_LOGFILE = "./crawllog.txt"
 
 def getPrices(urls : list) -> dict:
+    logfile = open(CRAWL_LOGFILE, 'a+')
+    now_time = datetime.now().strftime("%m/%d/%Y-%H:%M:%S")
+    logfile.write("\n------------------------------\n" + now_time + "\n------------------------------\n")
+
     prices = dict()
     for url in urls:
         response = requests.get(url)
-        print(response.status_code, "|", end=' ')
+        resp_str = str(response.status_code) + "| "
         if(response.status_code != 200):
-            print("Bad status while crawling", url)
+            resp_str += "Bad status while crawling" + url
+        print(resp_str)
+        logfile.write(resp_str)
         prices = parsePrices(response)
         if prices != {}:
             break
     if prices == {}:
-        print("Couldn't fetch prices for the provided urls:\n", urls)
+        price_error = "Couldn't fetch prices for the provided urls:\n" + str(urls)
+        print(price_error)
+        logfile.write(price_error)
+    logfile.close()
     return prices
 
 
@@ -33,8 +45,15 @@ def parsePrices(response : requests.models.Response) -> dict:
 
         # # prices are in the form "X,YY €". After matching a string to a price using the following regex,
         # # we remove the last 2 characters, replace the comma with a dot, and convert the string to a float
-        priceRegex = compile(r'[0-9]*\,[0-9]{1,2}\ .')
-        prices = [float(priceString[:-2].replace(',', '.')) for priceString in infoData if priceRegex.match(priceString)]
+        euroRegex = compile(r'[0-9]*\.[0-9]{1,2}\ €')
+        poundRegex = compile(r'£[0-9]*\.[0-9]{1,2}')
+        prices = []
+        for priceString in infoData:
+            priceString = priceString.replace(',', '.')
+            if euroRegex.match(priceString):
+                prices.append(float(priceString[:-2]))
+            elif poundRegex.match(priceString):
+                prices.append(float(priceString[1:]))
 
         try:
             return {
